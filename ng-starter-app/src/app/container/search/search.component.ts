1f5c034a0f9c77ddbd8b49b7fc1006044d5c297e
Akif Irfan <Akif.Irfan@stewart.com>
2025-02-05 22:31:04 +0500

    Lession 22 ngIf directive

--- ng-starter-app/src/app/container/search/search.component.ts
+++ ng-starter-app/src/app/container/search/search.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prod-search',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchText: string = 'Mens Wear';
+  searchText: string = '';
 }
