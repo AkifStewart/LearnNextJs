5d7b5fb2f8e73aaeb66938966cf1932f948f2c92
Akif Irfan <Akif.Irfan@stewart.com>
2025-02-13 19:00:59 +0500

    Lesson 30 - Template Reference variable  on component

--- ng-starter-app/src/app/container/product-list/product-list.component.ts
+++ ng-starter-app/src/app/container/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { FilterComponent } from "./filter/filter.component";
+import { Product } from '../../models/Product';
 
 @Component({
   selector: 'product-list',
@@ -10,6 +11,7 @@ import { FilterComponent } from "./filter/filter.component";
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  selectedProduct: Product;
   products = [
     {
       id: 1,
